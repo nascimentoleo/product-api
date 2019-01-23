762a71313ddaaaaa9492d6826d5b8ea23f7d7798
Leo Nascimento <leonardo_pinheiro12@hotmail.com>
2019-01-22 22:19:37 -0300

    feat: create product app and your models

---
+++ products/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.utils.timezone import now
+
+
+class Category(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=25)
+    created_at = models.DateTimeField(default=now, blank=True)
+    updated_at = models.DateTimeField(default=now, blank=True)
+
+
+class Product(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=25)
+    size = models.IntegerField()
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+    created_at = models.DateTimeField(default=now, blank=True)
+    updated_at = models.DateTimeField(default=now, blank=True)
+
